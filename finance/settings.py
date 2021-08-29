bc1c7b58fd651a54f17b7ef8045a5adac84cff84
Purvi Binani <purvi2401binani@gmail.com>
2021-08-29 12:08:14 +0530

    register user

--- finance/settings.py
+++ finance/settings.py
@@ -133,3 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
+
+# MESSAGE_TAGS = {
+#     messages.ERROR: 'danger'
+# }
