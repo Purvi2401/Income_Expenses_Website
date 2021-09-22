512d7ebe3cd5be56a1f980dfdb0d2199cdcd8e09
Purvi Binani <purvi2401binani@gmail.com>
2021-09-22 15:34:02 +0530

    added charts+minor changes

--- finance/settings.py
+++ finance/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'expenses',
-    'income'
-    'userpreferences'
+    'userincome',
+    'userpreferences',
 ]
 
 MIDDLEWARE = [
